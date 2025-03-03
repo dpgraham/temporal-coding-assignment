29bf05b5af2bb743485c69f45a7a3e55cededce8
Daniel Graham <dpgraham.85@gmail.com>
2025-03-03 12:01:46 -0800

    make it only show results from recent fetch

--- README.md
+++ README.md
@@ -1,5 +1,26 @@
 # TEMPORAL POKEMON TAKE HOME ASSIGNMENT
+* Open this page at index.html
+* Set "chaos=true" in query params to use chaos API calls
+* Set "flakey=true" in query params to make the API calls retry twice
 
 ## COMMENTS
-* Doing Vanilla HTML to prove that I still know how Javascript API's work and 
+* Doing Vanilla HTML to prove that I still know how Javascript/HTML API's work and 
   so I can avoid the bloat and boilerplate of adding a framework
+
+
+# ISSUES
+* Trying to add 'page=n' to the search gave me this error with a 500 status code
+
+```
+InvalidCharacterError
+    at /rbd/pnpm-volume/0e30c8f9-2d00-4981-bdf5-670eafdca7e7/node_modules/base-64/base64.js:23:36
+    at Object.<anonymous> (/rbd/pnpm-volume/0e30c8f9-2d00-4981-bdf5-670eafdca7e7/node_modules/base-64/base64.js:164:2)
+    at Module._compile (node:internal/modules/cjs/loader:1103:14)
+    at Object.Module._extensions..js (node:internal/modules/cjs/loader:1157:10)
+    at Module.load (node:internal/modules/cjs/loader:981:32)
+    at Function.Module._load (node:internal/modules/cjs/loader:822:12)
+    at ModuleWrap.<anonymous> (node:internal/modules/esm/translators:168:29)
+    at ModuleJob.run (node:internal/modules/esm/module_job:197:25)
+    at async Promise.all (index 0)
+    at async ESMLoader.import (node:internal/modules/esm/loader:337:24)
+```
--- index.js
+++ index.js
@@ -5,37 +5,91 @@
   const statusEl = document.getElementById('status');
   const API_ROOT = 'https://meowing-bristle-alamosaurus.glitch.me';
 
+  const urlParams = new URLSearchParams(window.location.search);
+  const chaos = urlParams.get('chaos');
+  const flakey = urlParams.get('flakey');
+
   // !COPY PASTED FROM https://www.freecodecamp.org/news/javascript-debounce-example/
+  // helper function to debounce searches by 300ms
   function debounce(func, timeout = 300){
     let timer;
-    return (...args) => {
+    const fn = (...args) => {
       clearTimeout(timer);
       timer = setTimeout(() => { func.apply(this, args); }, timeout);
     };
+    return fn;
   }
 
   function setStatus (status) {
     statusEl.innerHTML = status;
   }
 
-  function fetchPokemon (text) {
+  // keeps track of what the user's most recent search term was
+  // so if we return results from a different search, ignore it
+  let mostRecentSearch = null;
+
+  // fetches a list of pokemon that match text and then renders them in HTML
+  function fetchPokemon (text, retries=3) {
+    let searchUrl = `${API_ROOT}/api/pokemon/search/${text}?`;
+    if (chaos) searchUrl += 'chaos=true&';
+    if (flakey && retries > 1) {
+        searchUrl += 'flakiness=1&';
+    }
+    mostRecentSearch = text;
     if (text === '') {
       setStatus('Unrequested');
       return populatePokemon();
     }
     setStatus('Loading');
+    pokemonListEl.innerHTML = '';
     
     hasRunningFetch = true;
-    fetch(`${API_ROOT}/api/pokemon/search/${text}`)
+    fetch(searchUrl)
       .then((res) => {
+        if (mostRecentSearch !== text) return;
+        if (res.status === 404) {
+            setStatus('Empty Results');
+        } else if (res.status >= 500 && retries > 0) {
+            fetchPokemon(text, retries - 1);
+            return;
+        } else if (res.status > 400) {
+            setStatus('Error');
+        }
         res.json()
-          .then(({ pokemon }) => populatePokemon(pokemon));
-          // TODO: catch when something not working
+          .then(({ pokemon, nextPage }) => {
+            if (mostRecentSearch !== text) return;
+            populatePokemon(pokemon);
+            if (nextPage) {
+              fetchNextPage(text, 1);
+            }
+          })
       });
   }
 
+  // get the next page of Pokemon
+  // TODO: make this and fetchPokemon less repetitive
+  function fetchNextPage (text, page) {
+    let searchUrl = `${API_ROOT}/api/pokemon/search/${text}?page=${page}`;
+    if (chaos) searchUrl += '&chaos=true';
+    fetch(searchUrl + '')
+      .then((res) => {
+        if (mostRecentSearch !== text) return;
+        res.json()
+          .then(({ pokemon, nextPage }) => {
+            if (mostRecentSearch !== text) return;
+            populatePokemon(pokemon);
+            if (nextPage) {
+              fetchNextPage(text);
+            }
+          })
+          .catch((res) => console.log(res))
+      });
+  }
+
+  // debounce the searches so that there isn't a search on every single keystroke
   const fetchPokemonHandler = debounce(fetchPokemon);
 
+  // add a list of pokemons to the HTML
   function populatePokemon (pokemons) {
     if (!pokemons) {
       pokemonListEl.innerHTML = '';
